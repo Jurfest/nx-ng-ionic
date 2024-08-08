fb66db5f57374eb3a19b4c9aa92304b11127e4b4
Jurfest <jurfest.apps@gmail.com>
2024-08-07 23:09:47 -0300

    chore: connect todo domain into todo-mobile

--- apps/todo-mobile/src/app/app.config.ts
+++ apps/todo-mobile/src/app/app.config.ts
@@ -1,14 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
-import {
-  PreloadAllModules,
-  provideRouter,
-  RouteReuseStrategy,
-  withPreloading,
-} from '@angular/router';
-import {
-  IonicRouteStrategy,
-  provideIonicAngular,
-} from '@ionic/angular/standalone';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
+import { PreloadAllModules, provideRouter, RouteReuseStrategy, withPreloading } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideTodoDomain } from '@nx-ng-ionic/todo/domain';
 
 import { routes } from './app.routes';
 
@@ -17,5 +14,11 @@ export const appConfig: ApplicationConfig = {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptors([]), withFetch()),
+    provideStore(),
+    provideEffects([]),
+    ...(isDevMode() ? [provideStoreDevtools()] : []),
+    provideTodoDomain(),
   ],
 };
