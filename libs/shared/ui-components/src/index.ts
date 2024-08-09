d8416ac9f545948bc978ffd9c194fb3ed2fd86b7
Jurfest <jurfest.apps@gmail.com>
2024-08-09 15:05:17 -0300

    feat: add initial clients layout

--- libs/shared/ui-components/src/index.ts
+++ libs/shared/ui-components/src/index.ts
@@ -1,1 +1,3 @@
+export * from './lib/components/atoms/button/button.component';
+
 export * from './lib/components/organisms/navbar/navbar.component';
--- libs/todo/feature-client/src/lib/containers/web/client-web.component.ts
+++ libs/todo/feature-client/src/lib/containers/web/client-web.component.ts
@@ -1,16 +1,26 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule, TitleCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ButtonComponent } from '@nx-ng-ionic/shared/ui-components';
 
 import { ClientBaseComponent } from '../client-base.component';
+import { MatIconModule } from '@angular/material/icon';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'todo-client-web',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent, MatIconModule, ReactiveFormsModule, TitleCasePipe],
   templateUrl: './client-web.component.html',
   styleUrl: './client-web.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ClientWebComponent extends ClientBaseComponent {}
+export class ClientWebComponent extends ClientBaseComponent {
+  private fb = inject(FormBuilder);
+
+  clientForm = this.fb.group({
+    name: [''],
+    role: [''],
+  });
+}
 
 export default ClientWebComponent;
