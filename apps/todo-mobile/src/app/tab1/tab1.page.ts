3f8406c5ca558bd791f8e424bd1f6419d4fa83d6
Jurfest <jurfest.apps@gmail.com>
2024-08-08 23:39:26 -0300

    feat: basic tab1 layout

--- apps/todo-mobile/src/app/tab1/tab1.page.ts
+++ apps/todo-mobile/src/app/tab1/tab1.page.ts
@@ -1,10 +1,19 @@
+import { TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import {
+  IonButton,
+  IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { addCircle, create, trash } from 'ionicons/icons';
 
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
@@ -14,13 +23,43 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   styleUrls: ['tab1.page.scss'],
   standalone: true,
   imports: [
+    IonItem,
+    IonIcon,
+    IonButton,
+    IonButtons,
     IonHeader,
     IonToolbar,
     IonTitle,
     IonContent,
     ExploreContainerComponent,
+    IonIcon,
+    IonList,
+    IonList,
+    IonLabel,
+    TitleCasePipe,
   ],
 })
 export class Tab1Page {
-  constructor() {}
+  users = [
+    {
+      id: 1,
+      name: 'Nicolas Flamel',
+      role: 'admin',
+      avatar: 'Alchemist',
+    },
+    {
+      id: 2,
+      name: 'Thimbletack Pebble',
+      role: 'client',
+      avatar: 'Gnome',
+    },
+  ];
+
+  constructor() {
+    addIcons({ trash, create, addCircle });
+  }
+
+  addUser(): void {}
+  editUser(a: any): void {}
+  deleteUser(b: any): void {}
 }
--- apps/todo-mobile/src/app/tabs/tabs.page.ts
+++ apps/todo-mobile/src/app/tabs/tabs.page.ts
@@ -1,7 +1,13 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import {
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square } from 'ionicons/icons';
+import { list, people, codeSlash } from 'ionicons/icons';
 
 @Component({
   selector: 'nx-ng-ionic-tabs',
@@ -14,6 +20,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square });
+    addIcons({ list, people, codeSlash });
   }
 }
